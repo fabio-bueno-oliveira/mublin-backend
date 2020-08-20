7bc072bf1ab9be23682d5ba1c274aba774da4130
fabio bueno <fabiobueno@outlook.com>
2020-08-19 23:39:59 -0300

    Alterado o domínio no Access-Control

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://mublin.com');
+  res.header('Access-Control-Allow-Origin', 'https://www.mublin.com');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
