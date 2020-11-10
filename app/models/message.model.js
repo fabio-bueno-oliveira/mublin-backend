e109ddcfbbaecd52259b49fbe6c5a3abde82bf11
fabio bueno <fabiobueno@outlook.com>
2020-11-09 21:58:05 -0300

    Adicionada data em UNIX no endpoint de listar as conversações

--- app/models/message.model.js
+++ app/models/message.model.js
@@ -10,7 +10,7 @@ const Message = function(message) {
 
 Message.getAllConversations = (loggedID, result) => {
   let x = jwt.verify(loggedID.slice(7), process.env.JWT_SECRET)
-  sql.query(`SELECT m1.created AS lastMessageCreated, m1.id_user_from AS senderId, u1.name AS senderName, u1.lastname AS senderLastname, CONCAT('https://ik.imagekit.io/mublin/users/avatars/tr:h-200,w-200,c-maintain_ratio/',u1.id,'/',u1.picture) AS senderPicture, m1.id AS lastMessageId, m1.id_user_to AS receiverId, m1.message AS lastMessage, m1.seen AS lastMessageSeen FROM messages AS m1 INNER JOIN users AS u1 ON m1.id_user_from = u1.id INNER JOIN (SELECT id_user_from, MAX(created) AS MaxDate FROM messages WHERE id_user_to = ${x.result.id} GROUP BY id_user_from) AS m2 ON m1.id_user_from = m2.id_user_from AND m1.created = m2.MaxDate ORDER BY m1.created DESC
+  sql.query(`SELECT m1.created AS lastMessageCreated, UNIX_TIMESTAMP(m1.created) AS lastMessageCreatedFormatted, m1.id_user_from AS senderId, u1.name AS senderName, u1.lastname AS senderLastname, CONCAT('https://ik.imagekit.io/mublin/users/avatars/tr:h-200,w-200,c-maintain_ratio/',u1.id,'/',u1.picture) AS senderPicture, m1.id AS lastMessageId, m1.id_user_to AS receiverId, m1.message AS lastMessage, m1.seen AS lastMessageSeen FROM messages AS m1 INNER JOIN users AS u1 ON m1.id_user_from = u1.id INNER JOIN (SELECT id_user_from, MAX(created) AS MaxDate FROM messages WHERE id_user_to = ${x.result.id} GROUP BY id_user_from) AS m2 ON m1.id_user_from = m2.id_user_from AND m1.created = m2.MaxDate ORDER BY m1.created DESC
   `, (err, res) => {
     if (err) {
       result(err, null);
