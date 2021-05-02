1f05f8c1ffe6c054b20553ed21a8f65d713ba0e7
Fabio Bueno <fabio.boliveira@digisystem.com.br>
2021-05-02 18:54:53 -0300

    Adicionada query de tipos de equipamento em profile/:user/gear

--- app/models/db.js
+++ app/models/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
 
 var connection = mysql.createPool({
-  multipleStatements: false,
+  multipleStatements: true,
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
