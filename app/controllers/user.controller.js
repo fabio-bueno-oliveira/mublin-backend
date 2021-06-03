7b7e59a31c3d28f7eadf9cc193e8ee929e255342
Fabio Bueno <fabio.boliveira@digisystem.com.br>
2021-06-03 11:50:20 -0300

    Aumentada de 7 para 30 dias o tempo de expiração da sessão do login

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -66,7 +66,7 @@ exports.loginUser = (req, res) => {
     if (result) {
       results.password = undefined;
       const jsontoken = sign({ result: results }, process.env.JWT_SECRET, {
-        expiresIn: "7 days" 
+        expiresIn: "30 days" 
       });
       User.log(results.id, jsontoken, (err, data) => {
         if (err)
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -51,7 +51,7 @@ User.create = (newUser, result) => {
       status: 1
     }
     const jsontoken = sign({ result: results }, process.env.JWT_SECRET, {
-        expiresIn: "7 days" 
+        expiresIn: "30 days" 
     });
     result(null, { id: res.insertId, username: newUser.username, name: newUser.name, lastname: newUser.lastname, email: newUser.email, token: jsontoken });
 
